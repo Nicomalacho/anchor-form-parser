737cbe0c286d73a94703d1304f949416c9d80507
Nicolas  Gaviria Mejia <nicolasgaviriamejia@nicolass-mbp.lan>
2019-02-18 17:37:47 -0500

    added withdraw constanst

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ const AnchorParser = require('./lib/anchor-form-parser');
 testParser = async () => {
   try {
     const anchorParser = await AnchorParser('EURT', 'GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S', 'withdraw');
-    // const anchorParser = await AnchorParser('BTC', 'GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S', 'deposit');    
-    console.log(anchorParser.cash);
-    console.log(anchorParser.cash.schema.properties);
-    console.log(anchorParser.cash.options.fields);
+    // const anchorParser = await AnchorParser('BTC', 'GAUTUYY2THLF7SGITDFMXJVYH3LHDSMGEAKSBU267M2K7A3W543CKUEF', 'withdraw');    
+    console.log(anchorParser);
+    console.log(anchorParser.crypto.schema.properties);
+    console.log(anchorParser.crypto.options.fields);
   } catch (e) {
     console.log(e);
   }
